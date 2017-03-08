e39088599f04a5d5a3d53a35518ae1655ea7bf39
heepie <gheepie@gmail.com>
2017-03-08 23:30:01 +0900

    to modify activity_main

--- app/src/main/java/com/heepie/campusnavigation/Concrete/MainActivity.java
+++ app/src/main/java/com/heepie/campusnavigation/Concrete/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
